f9f7d8f0294abe1be747acdb7f8e9d561ef1a35d
Matthias Guenther <matthias@wikimatze.de>
2014-11-16 08:03:23 +0100

    Add shebang and better name for the installing script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 # The ouput of all these installation steps is noisy. With this utility
 # the progress report is nice and concise.
-function install {
-  echo installing $1
+function installing {
+  echo "installing $1"
   shift
   apt-get -y install "$@" >/dev/null 2>&1
 }
@@ -9,11 +11,11 @@ function install {
 echo "Updating package information"
 apt-get -y update >/dev/null 2>&1
 
-install 'development tools' build-essential
+installing 'development tools' build-essential
 
-install Git git
-install SQLite sqlite3 libsqlite3-dev
-install Vim vim
+installing 'Git vcm' git
+installing 'SQLite tool' sqlite3 libsqlite3-dev
+installing 'Vim editor' vim
 
 echo "Install Ruby"
 cd /tmp && rm -rf ruby-install && git clone https://github.com/postmodern/ruby-install.git >/dev/null 2>&1
