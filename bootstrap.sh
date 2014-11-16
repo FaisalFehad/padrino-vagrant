eaffd22d149da8d5d5c3d2f4425034a75f920225
Matthias Guenther <matthias@wikimatze.de>
2014-11-16 09:22:07 +0100

    Switch to ruby 2.1.5 and source chruby so that gems are installed correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,14 +19,19 @@ installing 'Vim editor' vim
 
 echo "Install Ruby"
 cd /tmp && rm -rf ruby-install && git clone https://github.com/postmodern/ruby-install.git >/dev/null 2>&1
-cd ruby-install && git checkout v0.5.0 >/dev/null 2>&1 && sudo make install >/dev/null 2>&1
+cd ruby-install && sudo make install >/dev/null 2>&1
 cd /tmp && rm -rf chruby && git clone https://github.com/postmodern/chruby.git >/dev/null 2>&1
 cd chruby && sudo make install >/dev/null 2>&1
-ruby-install ruby 2.1.3 >/dev/null 2>&1
+ruby-install ruby 2.1.5 >/dev/null 2>&1
 
 echo "source /usr/local/share/chruby/chruby.sh" >> /home/vagrant/.bashrc
-echo "chruby 2.1.3" >> /home/vagrant/.bashrc
+echo "chruby 2.1.5" >> /home/vagrant/.bashrc
 
-echo "Install bundle and padrino"
-gem install bundler --no-ri --no-rdoc
+# Switching to
+source /usr/local/share/chruby/chruby.sh
+chruby 2.1.5
+
+echo "Install bundler and padrino "
+gem install bundler --no-document
+gem install padrino
 
